f314718ee6f1e746df8f80137420cb7f7b11d51a
Vermasaiyam <vermasaiyam9@gmail.com>
2024-11-04 20:16:37 +0530

    user model

---
+++ backend/models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+}, { timestamps: true })
+
+module.exports = mongoose.model('User', userSchema);
